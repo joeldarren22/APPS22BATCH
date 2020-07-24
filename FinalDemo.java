c24fb7182f8c1300a0294ac605a74071a02b4b66
J3-Aquacool <42802228+J3-Aquacool@users.noreply.github.com>
2020-07-24 12:43:33 +0530

    Add files via upload
    finaldemo class added

---
+++ FinalDemo.java
@@ -0,0 +1,33 @@
+ final class Parent
+ {
+	
+void add()	
+{
+	
+	
+}
+	
+}
+class Child 
+{
+	void add()
+	{
+		
+		
+	}
+	
+}
+
+
+
+
+public class FinalDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Parent p=new Parent();
+		
+	}
+
+}
